3fdef23c2c49c5529c838ab2ce03dbd6fb10326a
William Bergqvist <william.bergqvist-konsult@apoteket.se>
2020-03-23 16:27:38 +0100

    Implement model, init, fetching, update. View remains. And prob some more.

--- src/App.fs
+++ src/App.fs
@@ -1,31 +1,78 @@
 module App
 
-type Model = int
-
-type Msg = Increment | Decrement
-
-
-open Elmish
-
-let init () =
-  0
-
-let update (msg:Msg) count =
-  match msg with
-  | Increment -> count + 1
-  | Decrement -> count - 1
-
+open Fable.SimpleHttp
+open Fable.SimpleJson 
 open Fable.React
 open Fable.React.Props
+open Elmish
+open Elmish.React
+
+type AgeGroup = 
+    {
+        age_group: string
+        count: int
+        percent: int
+    }
+
+type BirthAge = 
+    {
+        year: int
+        count: int
+    }
+
+type Model =
+    | Failure of string
+    | Loading
+    | Success of AgeGroup list
+
+type Msg = 
+    | LoadedAgeGroups of Result<AgeGroup list, string>
+    | LoadedBirthAges of Result<BirthAge list, string>
+
+
+let fetchDataByAgeGroup = async {
+    let! (statusCode, responseText) = Http.get "/FenixWeb/Data/R14/1137/ByAgeGroup/"
+
+    match statusCode with
+    | 200 -> return LoadedAgeGroups (Json.tryParseAs<AgeGroup list> responseText)
+    | _ -> return LoadedAgeGroups (Error responseText)
+}
+
+
+let fetchDataByBirthAge = async {
+    let! (statusCode, responseText) = Http.get "/FenixWeb/Data/R14/1137/"
+
+    match statusCode with
+    | 200 -> return LoadedBirthAges (Json.tryParseAs<BirthAge list> responseText)
+    | _ -> return LoadedBirthAges (Error responseText)
+}
+
+//TODO: Cmd batch and interop with JavaScript
+let init () =
+  Loading, Cmd.OfAsync.result fetchDataByAgeGroup
+
+let update (msg: Msg) (model: Model, cmd: Cmd<Msg>) : Model * Cmd<Msg> =
+    match msg with
+    | LoadedAgeGroups result ->
+        match result with
+        | Ok r14AgeGroups ->
+            (Success r14AgeGroups, Cmd.none)
+        | Error errorMsg ->
+            (Failure errorMsg, Cmd.none)
+    
+    | LoadedBirthAges result ->
+        match result with
+        | Ok r14BirthAges ->
+            (model, Cmd.none) //TODO: interop JavaScript
+        | Error errorMsg ->
+            (Failure errorMsg, Cmd.none)
 
 
 let view model dispatch =
   div []
-      [ button [ OnClick (fun _ -> dispatch Decrement) ] [ str "-" ]
-        div [] [ str (sprintf "%A" model) ]
-        button [ OnClick (fun _ -> dispatch Increment) ] [ str "+" ] ]
+      []
 
-open Elmish.React
+
 
 Program.mkSimple init update view
 |> Program.withReactBatched "elmish-app"
